4ef5ec07591f53745a7c57faf334b3cbd845fac6
Aleksander Kwiatkowski <bobikx@poczta.fm>
2019-01-27 11:40:36 +0100

    init

---
+++ src/kadu_exporter.cr
@@ -0,0 +1,6 @@
+require "./kadu_exporter/*"
+
+# TODO: Write documentation for `KaduExporter`
+module KaduExporter
+  # TODO: Put your code here
+end
---
+++ src/kadu_exporter/version.cr
@@ -0,0 +1,3 @@
+module KaduExporter
+  VERSION = "0.1.0"
+end
